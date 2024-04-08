57b7ca5bb888ac5100a58f7492c0a2759d197290
Areesha  Khan <160509293+Areeshakhan723@users.noreply.github.com>
2024-04-08 21:18:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+#! /usr/bin/env node    
+import inquirer from "inquirer";
+let todos = [];
+let conditions = true;
+while (conditions) {
+    let todoAns = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your Todos list?",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        },
+    ]);
+    // console.log(todoAns.todo);
+    todos.push(todoAns.todo);
+    console.log(todos);
+    conditions = todoAns.addMore;
+    if (!todoAns.addMore) {
+        let tododeleted = await inquirer.prompt([
+            {
+                name: "delete",
+                type: "confirm",
+                message: "you want to delete anything in todo? ",
+                default: "false",
+            },
+        ]);
+        if (tododeleted.delete) {
+            let deleteAns = await inquirer.prompt([
+                {
+                    name: "index",
+                    message: "Select an element which you want to delete and choices todos:",
+                    type: "list",
+                    choices: todos,
+                },
+            ]);
+            let index = todos.indexOf(deleteAns.index);
+            todos.splice(index, 1);
+            console.log(todos);
+            conditions = false;
+        }
+    }
+}
